fdd6a97360856afcbbdbab1b9c542b2c33283042
Jeremy Custenborder <jcustenborder@gmail.com>
2018-05-02 15:52:14 -0500

    Removed test for size vs batch size. Use count of elements removed for test instead of `isEmpty()` Fixes #71

--- connect-utils/src/test/java/com/github/jcustenborder/kafka/connect/utils/data/SourceRecordDequeTest.java
+++ connect-utils/src/test/java/com/github/jcustenborder/kafka/connect/utils/data/SourceRecordDequeTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,10 +26,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class SourceRecordDequeTest {
@@ -54,6 +56,13 @@ public class SourceRecordDequeTest {
     );
   }
 
+  @Test
+  public void empty() {
+    SourceRecordDeque deque = this.builder.build();
+    List<SourceRecord> records = deque.newList();
+    assertFalse(deque.drain(records, 100), "deque is empty. False should have been returned." );
+    verify(this.time, atLeastOnce()).sleep(100);
+  }
 
   @Test
   public void standard() {
