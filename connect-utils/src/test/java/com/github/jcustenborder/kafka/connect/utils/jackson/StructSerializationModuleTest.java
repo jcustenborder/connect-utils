e73b2cb8577c05fa61be473db1a2afd18c389596
Jeremy Custenborder <jcustenborder@gmail.com>
2019-01-22 14:06:34 -0600

    Corrected storage of field values. Fixes #132.

--- connect-utils/src/test/java/com/github/jcustenborder/kafka/connect/utils/jackson/StructSerializationModuleTest.java
+++ connect-utils/src/test/java/com/github/jcustenborder/kafka/connect/utils/jackson/StructSerializationModuleTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,26 +15,81 @@
  */
 package com.github.jcustenborder.kafka.connect.utils.jackson;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.io.PatternFilenameFilter;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static com.github.jcustenborder.kafka.connect.utils.AssertStruct.assertStruct;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 public class StructSerializationModuleTest {
   private static final Logger log = LoggerFactory.getLogger(StructSerializationModuleTest.class);
+
+  @BeforeAll
+  public static void beforeAll() {
+    ObjectMapperFactory.INSTANCE.configure(SerializationFeature.INDENT_OUTPUT, true);
+  }
+
+
+  public static class TestCase {
+
+
+  }
+
+  File testcaseRoot = new File("src/test/resources/com/github/jcustenborder/kafka/connect/utils/jackson");
+
+  @TestFactory
+  public Stream<DynamicTest> parse() {
+    File[] testFiles = testcaseRoot.listFiles(new PatternFilenameFilter("^struct\\d+\\.json$"));
+
+    return Arrays.stream(testFiles)
+        .map(f -> dynamicTest(f.getName(), () -> {
+          Struct struct = ObjectMapperFactory.INSTANCE.readValue(f, Struct.class);
+          assertNotNull(struct);
+
+        }));
+  }
+
+
+  @Test
+  public void foo() throws IOException {
+    File outputPath = new File("src/test/resources/com/github/jcustenborder/kafka/connect/utils/jackson");
+
+    File inputFile = new File(outputPath, "test.cases");
+    try (JsonParser parser = ObjectMapperFactory.INSTANCE.getJsonFactory().createParser(inputFile)) {
+      MappingIterator<ObjectNode> nodes = ObjectMapperFactory.INSTANCE.readValues(parser, ObjectNode.class);
+
+      int index = 1;
+      while (nodes.hasNext()) {
+        File outputFile = new File(outputPath, String.format("struct%03d.json", index));
+        ObjectNode node = nodes.next();
+        ObjectMapperFactory.INSTANCE.writeValue(outputFile, node);
+        index++;
+      }
+    }
+  }
+
 
   @TestFactory
   public Stream<DynamicTest> roundtrip() {
